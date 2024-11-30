5e6c25ad0c3a09ce639a3f6516bd6926347c7ebc
Biango Christian <biangochristian@MacBook-Air-de-Biango.local>
2024-11-30 19:42:37 +0100

    added favicons

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
+);
 
-workbow.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst() // PWA va essayer de récupérer l'image en cache avant de la récupérer sur le réseau. C'est ok pour les fichiers qui ne changent pas souvent comme les images. Donc, sinon utiliser une NetworkFirst() stratégie
-)
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.CacheFirst()
+);
