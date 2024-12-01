f62d2cfd770e97336ccff3edc476d5a861f50ec9
Biango Christian <biangochristian@MacBook-Air-de-Biango.local>
2024-12-01 01:48:31 +0100

    added page base skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
   content: ["./index.html", "./js/**/*.js"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#2A4566",
+      lightBlue: "#87C0EA",
+      white: "#FFFFFF",
+      opaqueWhite: "rgba(255, 255, 255, 0.2)",
+      pink: "#E37792",
+      gray: "#F3F5F7",
+      lightGray: "#D7D7D7",
+    },
+    fontFamily: {
+      sans: ['"public/fonts/Linotte"', "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
