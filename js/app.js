bb49da8102f60508d16e8ef7a9d08ba73114ac38
Biango Christian <biangochristian@MacBook-Air-de-Biango.local>
2024-12-01 23:11:28 +0100

    added scroll to section on button click

--- js/app.js
+++ js/app.js
@@ -7,4 +7,21 @@ document.addEventListener("alpine:init", () => {
       });
     },
   }));
+
+  Alpine.data("scrollHandler", () => ({
+    scrollToSection(event) {
+      const targetId = event.target
+        .closest("button")
+        .getAttribute("data-target");
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        const offset = 90;
+        const targetPosition =
+          targetSection.getBoundingClientRect().top + window.scrollY - offset;
+
+        window.scrollTo({ top: targetPosition, behavior: "smooth" });
+      }
+    },
+  }));
 });
